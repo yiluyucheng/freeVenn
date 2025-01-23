945e1451019dc8fa858ad619cc58e58fe6d90a67
yiluyucheng <wangyucheng511@gmail.com>
2025-01-23 07:56:13 +0000

    add linetype

--- examples/tutorial.R
+++ examples/tutorial.R
@@ -5,9 +5,8 @@ set.seed(123)  # Set the seed to ensures reproducibility
 
 vlist <- list(groupA=sample(1:100, 80), groupB=sample(50:150, 70), groupC=sample(1:100, 60))
 vlist2 <- list(groupA=sample(1:100, 80), groupB=sample(60:150, 40))
-vlist4 <- list(groupA=sample(1:100, 80), groupB=sample(50:150, 70), groupC=sample(1:100, 60), groupC=sample(40:200, 60))
+vlist4 <- list(groupA=sample(1:100, 80), groupB=sample(50:150, 70), groupC=sample(1:100, 60), groupD=sample(40:200, 60))
 
-freeVenn(vlist, weighted=TRUE)
 
 p1 <- freeVenn(vlist)
 p2 <- freeVenn(vlist, radii=0.7, alpha=0)
@@ -16,12 +15,12 @@ p3 <- freeVenn(vlist, radii=1.5, alpha=0.2, color=c('red', 'green', 'blue'), fon
 p4 <- freeVenn(vlist, weighted=TRUE) 
 p5 <- freeVenn(vlist, weighted=TRUE, color=c('red', 'green', 'blue'), fontface='bold') + 
   scale_fill_manual(values=c('#ff70a6', '#70d6ff', '#ffd670'))
-p6 <- freeVenn(vlist, weighted=TRUE, Optimize=TRUE, linewidth=1.2, alpha=0) +
+p6 <- freeVenn(vlist, weighted=TRUE, Optimize=TRUE, linewidth=1.2, linetype=2, alpha=0) +
     scale_color_manual(values=c('#ff70a6', '#70d6ff', '#ffd670'))
     
 p7 <- freeVenn(vlist2)   
 p8 <- freeVenn(vlist2, weighted=TRUE, linewidth=0)  
-p9 <- freeVenn(vlist4, alpha=0.3)    
+p9 <- freeVenn(vlist4, alpha=0.2)    
 
 plot_grid(p1, p2, p3, p4, p5, p6, p7, p8, p9, ncol = 3, labels = "auto")
 
